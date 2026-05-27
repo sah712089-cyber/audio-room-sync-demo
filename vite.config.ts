1ad1834a8156b3246fe698a42eb0bd46045d29c1
Akash Tiwari <tiwariboss32@gmail.com>
2026-05-28 00:04:13 +0530

    push

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   nitro: {
     preset: "vercel",
+    output: {
+      dir: ".vercel/output",
+      serverDir: ".vercel/output/functions/__server.func",
+      publicDir: ".vercel/output/static",
+    },
   },
   tanstackStart: {
     server: { entry: "server" },
